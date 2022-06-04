b5262a53dc815a4cbfd15cc23543409e2cc15f2e
swapnilag78 <swapnil.ag2001@gmail.com>
2022-06-04 20:34:50 +0530

    Exercise2 Done!

---
+++ exer2.js
@@ -0,0 +1,33 @@
+console.log('The quote \'There is no exercise better for the heart than reaching down and lifting people up.\' by John Holmes teaches us to \help\ one another.');
+
+
+console.log('\"Love is not patronizing and charity isn\'\ t about pity, it is about love. Charity and love are the same -- with charity you give love, so don\'\ t just give money but reach out your hand instead.\"');
+
+let num = 10;
+
+let type10_1 = typeof('10');
+let type10_2 = typeof(10);
+console.log(typeof('10'))
+console.log(typeof(10))
+
+// console.log(type10_1.equal(type10_2));
+
+
+console.log(Math.floor(Math.random() * 101));
+console.log(Math.floor(Math.random() * (51) + 50));
+console.log(Math.floor(Math.random() * 256));
+
+console.log(Math.random().toString(36).slice(2))
+
+let sentence1 = 'You cannot end a sentence with because because because is a conjunction';
+console.log(sentence1.indexOf('because'));
+console.log(sentence1.lastIndexOf('because'));
+
+console.log(sentence1.substring(31, 55));
+
+
+console.log('1 \t1 \t1 \t1 \t1')
+console.log('2 \t1 \t2 \t4 \t8')
+console.log('3 \t1 \t3 \t9 \t27')
+console.log('4 \t1 \t4 \t16 \t64')
+console.log('5 \t1 \t5 \t25 \t125')
