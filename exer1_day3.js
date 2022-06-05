fb3db3f61dfa96cb1d0c099618cd7c5905794e23
swapnilag78 <swapnil.ag2001@gmail.com>
2022-06-05 15:48:46 +0530

    day3 level 1 Done!

---
+++ exer1_day3.js
@@ -0,0 +1,29 @@
+let firstName = 'Swapnil'
+let lastName = 'Agrawal'
+let country = 'India'
+let city = 'Prayagraj'
+let age = 21
+let isMarried = false
+
+console.log(typeof(firstName))
+console.log(typeof(lastName))
+console.log(typeof(country))
+console.log(typeof(city))
+console.log(typeof(age))
+console.log(typeof(isMarried))
+
+console.log(typeof('10') == typeof(10))
+
+console.log(parseInt('10') == 10)
+
+console.log(firstName)
+console.log(lastName)
+console.log(10)
+
+
+console.log(0)
+console.log(NaN)
+console.log(false)
+
+console.log(4 === '4')
+console.log(!(4 === '4')
