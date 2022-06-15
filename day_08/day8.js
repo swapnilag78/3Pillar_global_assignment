00f90d104c79f346ba63b23cdfca9f083faa0be0
Swapnil Agrawal <swapnil.ag2001@gmail.com>
2022-06-15 21:25:06 +0530

    local to github javascript files!

--- day_08/day8.js
+++ day_08/day8.js
@@ -1,94 +1,20 @@
-/*
-Exercises: Level 1
-Create an empty object called dog
-Print the the dog object on the console
-Add name, legs, color, age and bark properties for the dog object. The bark property is a method which return woof woof
-Get name, legs, color, age and bark value from the dog object
-Set new properties the dog object: breed, getDogInfo
-*/
 let dog = {};
 console.log(dog);
-dog.name = "milli";
+dog.name = "Bora";
 dog.legs = 4;
-dog.color = "golden"
+dog.color = "white"
 dog.age = 2;
-dog.bark = function () {
+dog.bark = function() {
     console.log("woof woof woof")
 }
 console.log(Object.values(dog));
 dog.breed = "golden retreiver";
-dog.getDogInfo = function () {
+dog.getDogInfo = function() {
     console.log(Object.values(this));
 }
 
 
-/*
-Exercises: Level 2
-Find the person who has many skills in the users object.
 
-Count logged in users, count users having greater than equal to 50 points from the following object.
-
-const users = {
-  Alex: {
-    email: 'alex@alex.com',
-    skills: ['HTML', 'CSS', 'JavaScript'],
-    age: 20,
-    isLoggedIn: false,
-    points: 30
-  },
-  Asab: {
-    email: 'asab@asab.com',
-    skills: ['HTML', 'CSS', 'JavaScript', 'Redux', 'MongoDB', 'Express', 'React', 'Node'],
-    age: 25,
-    isLoggedIn: false,
-    points: 50
-  },
-  Brook: {
-    email: 'daniel@daniel.com',
-    skills: ['HTML', 'CSS', 'JavaScript', 'React', 'Redux'],
-    age: 30,
-    isLoggedIn: true,
-    points: 50
-  },
-  Daniel: {
-    email: 'daniel@alex.com',
-    skills: ['HTML', 'CSS', 'JavaScript', 'Python'],
-    age: 20,
-    isLoggedIn: false,
-    points: 40
-  },
-  John: {
-    email: 'john@john.com',
-    skills: ['HTML', 'CSS', 'JavaScript', 'React', 'Redux', 'Node.js'],
-    age: 20,
-    isLoggedIn: true,
-    points: 50
-  },
-  Thomas: {
-    email: 'thomas@thomas.com',
-    skills: ['HTML', 'CSS', 'JavaScript', 'React'],
-    age: 20,
-    isLoggedIn: false,
-    points: 40
-  },
-  Paul: {
-    email: 'paul@paul.com',
-    skills: ['HTML', 'CSS', 'JavaScript', 'MongoDB', 'Express', 'React', 'Node'],
-    age: 20,
-    isLoggedIn: false,
-    points: 40
-  }
-}```
-Find people who are MERN stack developer from the users object
-
-Set your name in the users object without modifying the original users object
-
-Get all keys or properties of users object
-
-Get all the values of users object
-
-Use the countries object to print a country name, capital, populations and languages.
-*/
 
 const users = {
     Alex: {
@@ -163,105 +89,24 @@ let country = {
 console.log(`${country.name}\n${country.capital}\n${country.population}\n${country.languages}`)
 
 
-/*
-Exercises: Level 3
-Create an object literal called personAccount. It has firstName, lastName, incomes, expenses properties and it has totalIncome, totalExpense, accountInfo,addIncome, addExpense and accountBalance methods. Incomes is a set of incomes and its description and expenses is a set of incomes and its description.
-**** Questions:2, 3 and 4 are based on the following two arrays:users and products ()
-    const users = [
-    {
-        _id: 'ab12ex',
-        username: 'Alex',
-        email: 'alex@alex.com',
-        password: '123123',
-        createdAt:'08/01/2020 9:00 AM',
-        isLoggedIn: false
-    },
-    {
-        _id: 'fg12cy',
-        username: 'Asab',
-        email: 'asab@asab.com',
-        password: '123456',
-        createdAt:'08/01/2020 9:30 AM',
-        isLoggedIn: true
-    },
-    {
-        _id: 'zwf8md',
-        username: 'Brook',
-        email: 'brook@brook.com',
-        password: '123111',
-        createdAt:'08/01/2020 9:45 AM',
-        isLoggedIn: true
-    },
-    {
-        _id: 'eefamr',
-        username: 'Martha',
-        email: 'martha@martha.com',
-        password: '123222',
-        createdAt:'08/01/2020 9:50 AM',
-        isLoggedIn: false
-    },
-    {
-        _id: 'ghderc',
-        username: 'Thomas',
-        email: 'thomas@thomas.com',
-        password: '123333',
-        createdAt:'08/01/2020 10:00 AM',
-        isLoggedIn: false
-    }
-    ];
 
-    const products = [
-  {
-    _id: 'eedfcf',
-    name: 'mobile phone',
-    description: 'Huawei Honor',
-    price: 200,
-    ratings: [
-      { userId: 'fg12cy', rate: 5 },
-      { userId: 'zwf8md', rate: 4.5 }
-    ],
-    likes: []
-  },
-  {
-    _id: 'aegfal',
-    name: 'Laptop',
-    description: 'MacPro: System Darwin',
-    price: 2500,
-    ratings: [],
-    likes: ['fg12cy']
-  },
-  {
-    _id: 'hedfcg',
-    name: 'TV',
-    description: 'Smart TV:Procaster',
-    price: 400,
-    ratings: [{ userId: 'fg12cy', rate: 5 }],
-    likes: ['fg12cy']
-  }
-]
-Imagine you are getting the above users collection from a MongoDB database. a. Create a function called signUp which allows user to add to the collection. If user exists, inform the user that he has already an account.
-b. Create a function called signIn which allows user to sign in to the application
-
-The products array has three elements and each of them has six properties. a. Create a function called rateProduct which rates the product b. Create a function called averageRating which calculate the average rating of a product
-
-Create a function called likeProduct. This function will helps to like to the product if it is not liked and remove like if it was liked.
-
-*/
+*
+/
 
 let personAccount = {
-    firstName: "vansh",
-    lastName: "bansal",
+    firstName: "Swapnil ",
+    lastName: "Agrawal",
     income: {
-        freelancing: 15000,
-        internship: 20000
+        freelancing: 12000,
+        internship: 18000
     },
     expenses: {
-        internet: 500,
+        internet: 2000,
         food: 1000,
-        transportation: 500
+        transportation: 1000
     },
 
-    totalIncome: function () {
+    totalIncome: function() {
         let totIncome = 0;
         let values = Object.values(this.income);
         for (let i = 0; i < values.length; i++) {
@@ -269,18 +114,18 @@ let personAccount = {
         }
         return totIncome;
     },
-    totalExpenses: function () {
+    totalExpenses: function() {
         let totExpenses = 0;
         let values = Object.values(this.expenses);
         for (let i = 0; i < values.length; i++) {
-            totExpenses = totExpenses+ values[i]
+            totExpenses = totExpenses + values[i]
         }
         return totExpenses
     },
-    accountBalance: function () {
+    accountBalance: function() {
         return this.totalIncome() - this.totalExpenses();
     },
-    accountInfo: function () {
+    accountInfo: function() {
         return `name:${this.firstName} ${this.lastName}\n
                 incomes:${Object.entries(this.income)}
                 expenses: ${
@@ -342,16 +187,13 @@ const users = [{
 
 const signUp = (username, email, password) => {
     users.forEach(user => {
-        if (user.username === username && user.password === password)
-        {
+        if (user.username === username && user.password === password) {
             console.log("you already have an account")
-        } else 
-        {
+        } else {
             let date = new Date()
             let chars = "abcdefghiklmnopqrstuvwxyz";
             let id = [];
-            for (let i = 0; i < 6; i++) 
-            {
+            for (let i = 0; i < 6; i++) {
                 id.push(chars[Math.floor(Math.random() * chars.length)])
             }
             id = id.join("");
@@ -368,16 +210,11 @@ const signUp = (username, email, password) => {
     console.log(users[users.length - 1]);
 }
 
-const signIn = (username, password) =>
- {
-    for (let i = 0; i < users.length; i++) 
-    {
-        if (users[i].username.toLowerCase() === username.toLowerCase() && users[0].password === password) 
-        {
+const signIn = (username, password) => {
+    for (let i = 0; i < users.length; i++) {
+        if (users[i].username.toLowerCase() === username.toLowerCase() && users[0].password === password) {
             return users[i];
-        } 
-        else
-        {
+        } else {
             return "wrong username or password";
         }
     }
@@ -420,8 +257,7 @@ const products = [{
     }
 ]
 
-const rateProduct = (name, rate) =>
- {
+const rateProduct = (name, rate) => {
     products.forEach(product => {
         if (product.name.toLowerCase().includes(name)) {
             let chars = "0123456789abcdefghiklmnopqrstuvwxyz";
@@ -479,4 +315,3 @@ const likeProduct = (name) => {
 rateProduct("tv", "3.5");
 avgRating("mobile phone");
 likeProduct("Laptop");
-
